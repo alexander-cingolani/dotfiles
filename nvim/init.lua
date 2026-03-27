2148304d853c4c9eae8b7f5b68d6fb0cbdd78560
Alex Cingolani <alexander.cingolani05@gmail.com>
2026-03-27 17:17:01 +0000

    Fix autosave script

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,41 +17,49 @@ vim.opt.smartindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.clipboard = "unnamedplus"
+vim.opt.undofile = true
+vim.opt.updatetime = 1000
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
--- 4. BASIC KEYBINDS
+
+-- 4. AUTOCOMMANDS
 -- ==========================================
 
-vim.keymap.set("n", "<leader>p", function()
-    vim.fn.jobstart({ "xdg-open", vim.fn.expand("%:p") })
-end, { desc = "Open file in browser" })
-
--- 5. AUTOCOMMANDS
--- ==========================================
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "markdown", "text" },
-    callback = function()
-        vim.opt_local.wrap = true
-        vim.opt_local.linebreak = true
-        vim.opt_local.breakindent = true
-    end,
+vim.api.nvim_create_autocmd({ "FocusLost", "BufLeave" }, {
+	callback = function()
+		if vim.bo.modified and vim.bo.buftype == "" then
+			vim.api.nvim_command("silent! update")
+		end
+	end,
 })
 
--- 6.
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "text" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.linebreak = true
+		vim.opt_local.breakindent = true
+	end,
+})
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.loop or vim.uv).fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
+
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-    },
-    rocks = {
-        hererocks = false,
-    }
+	spec = {
+		{ import = "plugins" },
+	},
+	rocks = {
+		hererocks = false,
+	},
 })
